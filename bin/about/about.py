6bc64bd4a820826f0e9574774a6ad05c2d78ab34
Mikael Jacobson <mikael@mludd.se>
2015-02-28 11:55:26 +0100

    Added some sound assets and an --about/-a flag for the program.

---
+++ bin/about/about.py
@@ -0,0 +1,13 @@
+class About:
+	def print_about_message(self):
+		print """Pusher - a top-down shooter
+Code:
+	Mikael Jacobson <mikael@mludd.se>
+Graphics:
+	Mikael Jacobson <mikael@mludd.se>
+SFX:
+	Mikael Jacobson <mikael@mludd.se>
+Music:
+	\"Adventure Meme\" Kevin MacLeod (incompetech.com)
+	Licensed under Creative Commons: By Attribution 3.0
+	http://creativecommons.org/licenses/by/3.0/"""
--- bin/pusher.py
+++ bin/pusher.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os, sys, pygame
+import about
 import game
+
+if sys.argv[1:]:
+	if sys.argv[1] == "--about" or sys.argv[1] == "-a":
+		ab = about.About()
+		ab.print_about_message()
+		exit()
 
 instance = game.Game()
 exit(instance.run())
